88901e9c4a7efb4736b549722d2006f1580b7810
SSG <gssenyonjo@gmail.com>
2023-02-16 13:42:25 +0300

    Created a snackbar

--- ch1_intro/lib/friends_list.dart
+++ ch1_intro/lib/friends_list.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FriendsList extends StatelessWidget {
   List<Map> fetchPeople() {
     return [
       {"first": "Jack", "last": "Bauer"},
       {"first": "Tom", "last": "Landers"},
-      {"first":"Angel", "last":"Michael"}
+      {"first": "Angel", "last": "Michael"}
     ];
   }
 
@@ -16,14 +17,25 @@ class FriendsList extends StatelessWidget {
       children: _friends
           .map((person) => GestureDetector(
               onTap: () {
-                _friends.remove(person);
+                String msg = "${person['first']} ${person['last']}";
+                final SnackBar sb = SnackBar(
+                  content: Text(msg),
+                  duration: Duration(seconds: 5),
+                  action: SnackBarAction(
+                    textColor: Colors.white,
+                    label: "YEY!",
+                    onPressed: (){ print("Yeyyed!"); },
+                  )
+                );
+                ScaffoldMessenger.of(context).showSnackBar(sb);
+                // Scaffold.of(context);
+
                 print("tapped");
               },
-              child:
-                  Dismissible(
-                    key: Key(person['first']),
-                    child: Person(firstName: person['first'], lastName: person['last'])))
-          )
+              child: Dismissible(
+                  key: Key(person['first']),
+                  child: Person(
+                      firstName: person['first'], lastName: person['last']))))
           .toList(),
     );
   }
@@ -37,9 +49,9 @@ class Person extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8), 
-      decoration:const BoxDecoration(border:  Border(bottom:  BorderSide(width: 1))),
-      child: Text("I am $firstName $lastName")
-    );
+        padding: const EdgeInsets.all(8),
+        decoration:
+            const BoxDecoration(border: Border(bottom: BorderSide(width: 1))),
+        child: Text("I am $firstName $lastName"));
   }
 }
---
+++ ch1_intro/lib/my_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyLayout extends StatelessWidget {
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // drawer: LayoutDraw,
+    );
+  }
+}
