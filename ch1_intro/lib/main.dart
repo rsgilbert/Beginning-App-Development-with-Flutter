44943f0c018724a3115145f7d2c850cd7b71eae3
SSG <gssenyonjo@gmail.com>
2023-02-16 12:21:43 +0300

    looked at forms and form validation

--- ch1_intro/lib/main.dart
+++ ch1_intro/lib/main.dart
@@ -17,45 +17,83 @@ class Person extends StatelessWidget {
 }
 
 class FancyHelloWidget extends StatelessWidget {
-  String _searchTerm = "";
-  final TextEditingController _controller = TextEditingController(text: "");
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("A Fancy Hello App")),
-        body: Container(
-            alignment: Alignment.center,
-            child: Column(children: [
-              Person("Peterson", firstName: "Joyce", lastName: "Namuli"),
-              TextField(
-                controller: _controller,
-                onChanged: (String val) => _searchTerm = val,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp('[1-5]')),
-                  FilteringTextInputFormatter.deny(RegExp('2')),
-                  LengthLimitingTextInputFormatter(4)
-                ],
-                decoration: InputDecoration(
-                    labelText: "Credit Card", counterText: "ABC"),
-              ),
-              Checkbox(value: false, onChanged: (val) => print('Val is $val')),
-              Switch(
-                value: true,
-                onChanged: (val) => print('switch new val is $val'),
-              ),
-              MyRadioWidget(),
-              MySliderWidget(),
-              MyDropdownMenu()
-            ])),
+        body: Container(alignment: Alignment.center, child: MyForm()),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.thumb_up),
           onPressed: () => {},
         ),
       ),
     );
+  }
+}
+
+class MyForm extends StatelessWidget {
+  GlobalKey<FormState> _key = GlobalKey<FormState>();
+  String _searchTerm = "";
+  final TextEditingController _controller = TextEditingController(text: "");
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _key,
+        autovalidateMode: AutovalidateMode.always,
+        
+        child: Column(children: [
+          TextFormField(
+            controller: _controller,
+            validator: (value) {
+              if(value != null && value.length < 5) { return "Too short"; }
+              return null;
+            },
+          ),
+          FormField(
+            builder: (state) {
+              return Checkbox(
+                value: false, 
+                onChanged: (val) {
+                  _key.currentState?.validate();
+                }
+              );
+            }
+          )
+          // FormField<String>(
+          //   builder: (state) {
+          //     return TextField(
+          //       controller: _controller,
+          //       onChanged: (String val) => _searchTerm = val,
+          //       keyboardType: TextInputType.number,
+          //       inputFormatters: [
+          //         FilteringTextInputFormatter.allow(RegExp('[1-5]')),
+          //         FilteringTextInputFormatter.deny(RegExp('2')),
+          //         LengthLimitingTextInputFormatter(4)
+          //       ],
+          //       decoration:
+          //           InputDecoration(labelText: "Credit Card", counterText: "ABC"),
+          //     );
+          //   },
+          //   onSaved: (initialValue) {
+          //     print("Saved. Initial value is $initialValue");
+          //   },
+          //   validator: (val) {
+          //     print("Validating $val");
+          //     return "bad bad";
+          //   },
+          // ),
+          
+          // Checkbox(value: false, onChanged: (val) => print('Val is $val')),
+          // Switch(
+          //   value: true,
+          //   onChanged: (val) => print('switch new val is $val'),
+          // ),
+          // MyRadioWidget(),
+          // MySliderWidget(),
+          // MyDropdownMenu()
+        ]));
   }
 }
 
@@ -67,10 +105,15 @@ class MyDropdownMenu extends StatelessWidget {
     return DropdownButton<ClothSize>(
         value: _clothSize,
         items: <DropdownMenuItem<ClothSize>>[
-          DropdownMenuItem(child: Text(ClothSize.SMALL.name), value: ClothSize.SMALL),
-          DropdownMenuItem(child: Text(ClothSize.MEDIUM.name), value: ClothSize.MEDIUM),
-          DropdownMenuItem(child: Text(ClothSize.LARGE.name), value: ClothSize.LARGE),
-          DropdownMenuItem(child: Text(ClothSize.EXTRALARGE.name), value: ClothSize.EXTRALARGE),
+          DropdownMenuItem(
+              child: Text(ClothSize.SMALL.name), value: ClothSize.SMALL),
+          DropdownMenuItem(
+              child: Text(ClothSize.MEDIUM.name), value: ClothSize.MEDIUM),
+          DropdownMenuItem(
+              child: Text(ClothSize.LARGE.name), value: ClothSize.LARGE),
+          DropdownMenuItem(
+              child: Text(ClothSize.EXTRALARGE.name),
+              value: ClothSize.EXTRALARGE),
         ],
         onChanged: (val) => {});
   }
