b360015f67b5d9289a5840737dab9b400cfa57ea
SSG <gssenyonjo@gmail.com>
2023-02-19 14:01:00 +0300

    Looked at making http requests

--- ch10_beyond/lib/fut.dart
+++ ch10_beyond/lib/fut.dart
@@ -2,32 +2,25 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class HeadlessWidget extends StatelessWidget {
-
   @override 
   Widget build(BuildContext context) {
     play();
-    return Center(child: Text("X"));
+    return const Center(child: Text("X"));
   }
 }
 
 void play() async {
   try {
-    String data = await rootBundle.loadString('assets/database.json');
-    debugPrint(data);
-    var m = jsonDecode(data);
-    print("name is ${m['name']}");
+    Uri url = Uri.parse('http://localhost:4001?filename=Budgets & pies');
+    print("uri is $url");
+    print("encoded uri is ${Uri.encodeComponent('Budgets&Salaries')}");
+    var response = await get(url);
+    print("response is ${response.body}");
 
-    var mp2 = { 'first': 'Jack', 'last': 'Bauer'};
-    var s = json.encode(mp2);
-    debugPrint(s);
-    SharedPreferences sp = await SharedPreferences.getInstance();
-    sp.setInt('age', 25);
-    int? meaning = sp.getInt('age');
-    print('meaning of life is $meaning');
 
 
 
