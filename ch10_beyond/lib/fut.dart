dc8689e0d5c791e588de8e3fcf25ba470c1044c5
SSG <gssenyonjo@gmail.com>
2023-02-19 11:44:22 +0300

    looked at shared preferences and reading json files

--- ch10_beyond/lib/fut.dart
+++ ch10_beyond/lib/fut.dart
@@ -1,11 +1,38 @@
-void main() async {
-  Future myFuture = new Future(() {
-    print("Hey you!");
-    return 1;
-  });
-  myFuture.then((value) {
-    print("The value is $value");
-  });
-  int n = await myFuture;
-  print("n is $n");
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class HeadlessWidget extends StatelessWidget {
+
+  @override 
+  Widget build(BuildContext context) {
+    play();
+    return Center(child: Text("X"));
+  }
+}
+
+void play() async {
+  try {
+    String data = await rootBundle.loadString('assets/database.json');
+    debugPrint(data);
+    var m = jsonDecode(data);
+    print("name is ${m['name']}");
+
+    var mp2 = { 'first': 'Jack', 'last': 'Bauer'};
+    var s = json.encode(mp2);
+    debugPrint(s);
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setInt('age', 25);
+    int? meaning = sp.getInt('age');
+    print('meaning of life is $meaning');
+
+
+
+  } catch (e) {
+    print('Error $e');
+    rethrow;
+  }
 }
